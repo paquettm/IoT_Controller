2c76f4dd2a8c32df8143c3f07218fb654b4999f9
Michel Paquette <paquettm@vaniercollege.qc.ca>
2023-11-06 15:44:27 -0500

    Web interface for historian started

--- web_service/app.py
+++ web_service/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+import sqlite3
 
 app = Flask(__name__)
+DB_FILE = "../historian_data.db"
 
 # return a view file
 @app.route('/', methods=['GET'])
@@ -18,5 +20,28 @@ def service():
     data = [1,2,3,4]
     return jsonify(data)
 
+#get historian data to the web "user"
+@app.route('/historian', methods=['GET'])
+def historian():
+    topic = request.args.get('topic',"#")
+    timestamp = request.args.get('timestamp',"1970-01-01 00:00:00")
+
+    # todo replace the # by the SQL wildcard (%)
+#    topic = topic.replace("#",'\%')
+
+    print(topic)
+
+    SQL = "SELECT timestamp, topic, payload FROM historian_data WHERE timestamp >= ? and topic LIKE ?"
+
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute(SQL,(timestamp,topic))
+
+    results = cursor.fetchall()
+
+    result_data = [{"topic":row[1], "payload":row[2], "timestamp":row[0]} for row in results]
+
+    return jsonify(result_data)
+
 if __name__ == '__main__':
-    app.run() #this will only accept requests from localhost...
+    app.run(host='0.0.0.0', port=3000)
