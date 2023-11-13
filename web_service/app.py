ab71cb6acbf45fac66035b824f3649b3b136af3b
Michel Paquette <paquettm@vaniercollege.qc.ca>
2023-11-13 15:33:50 -0500

    added a configuration interface

--- web_service/app.py
+++ web_service/app.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect
 import sqlite3
+import json
 
 app = Flask(__name__)
 DB_FILE = "../historian_data.db"
+
+CONFIG_FILE = "../config.json"
+#load the configuration data
+with open(CONFIG_FILE, 'r') as config_file:
+    config_data = json.load(config_file)
 
 # return a view file
 @app.route('/', methods=['GET'])
@@ -10,15 +16,41 @@ def index():
     return render_template('index.html')
 
 # return simple text
-@app.route('/example', methods=['GET'])
-def example():
-    return 'example'
+@app.route('/configure', methods=['GET'])
+def configure():
+    return render_template('configure.html', config=config_data)
 
 # return JSON from a data source such as a database
-@app.route('/service',methods=['GET'])
-def service():
-    data = [1,2,3,4]
-    return jsonify(data)
+@app.route('/add_configuration',methods=['POST'])
+def add_configuration():
+    topic = request.form.get('topic')
+    comparison = request.form.get('comparison')
+    value = request.form.get('value')
+    result_topic = request.form.get('result_topic')
+    result_value = request.form.get('result_value')
+
+    input = {"conditions":[{"topic":topic, "comparison":comparison, "value":value}],"results":[{"topic":result_topic,"value":result_value}]}
+
+    config_data.append(input)
+
+    #rewrite the configuration file
+    with open(CONFIG_FILE, 'w') as config_file:
+        json.dump(config_data, config_file, indent=4)
+
+    return redirect('/configure')
+
+@app.route('/delete_configuration',methods=['POST'])
+def delete_configuration():
+    index = int(request.form.get('rule_to_delete'))
+
+    if 0 <= index and index < len(config_data):
+        del config_data[index]
+
+        #rewrite the configuration file
+        with open(CONFIG_FILE, 'w') as config_file:
+            json.dump(config_data, config_file, indent=4)
+
+    return redirect('/configure')
 
 #get historian data to the web "user"
 @app.route('/historian', methods=['GET'])
